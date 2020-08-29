0e9ca88819858809f9349aa7c471c070426be024
Thibaut Van Callemont <vancallemont.thibaut@gmail.com>
2020-08-29 21:54:17 +0200

    Maquette HTML

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disney Movies</title>
+</head>
+<body>
+    <h1><a href="./index.php">Disney Movies</a></h1>
+    <div class="moviesList">
+        <div class="movie">
+            <img class="moviePicture" src="./img/Cars 1.jpg" />
+            <h2 class="moveTitle">Cars</h2>
+            <p class="moveDate">2006</p>
+            <a href="./movie.php?id=1">Plus d'infos...</a>
+        </div>
+        <div class="movie">
+            <img class="moviePicture" src="./img/Toy story 1.jpg" />
+            <h2 class="moveTitle">Toy story</h2>
+            <p class="moveDate">1993</p>
+            <a href="./movie.php?id=2">Plus d'infos...</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ movie.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disney Movies</title>
+</head>
+<body>
+    <h1><a href="./index.php">Disney Movies</a></h1>
+    <h2>Cars</h2>
+    <p>2006</p>
+    <img src="./img/Cars 1.jpg" />
+    <div class="movieCharacters">
+        <div class="character">
+            <img src="./img/characters/Flash_McQueen.jpg" />
+            <h3>Flash McQueen</h3>
+            <p>Flash McQueen est un champion de Nascar, il se considère comme un "instrument de précision et un bolide aérodynamique".</p>
+        </div>
+        <div class="character">
+            <img src="./img/characters/Flash_McQueen.jpg" />
+            <h3>Martin</h3>
+            <p>Martin est le meilleur ami de Flash McQueen.</p>
+        </div>
+    </div>
+
+</body>
+</html>
