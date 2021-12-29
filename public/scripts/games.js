bfe37b67e386c4574b0e263a9a6ae2ccbd380967
Thibaut Van Callemont <vancallemont.thibaut@gmail.com>
2021-12-29 15:56:11 +0100

    Finalization of the memory game

--- public/scripts/games.js
+++ public/scripts/games.js
@@ -3,21 +3,30 @@ const levels = document.getElementsByName('level');
 const startBtn = document.getElementById('startGame');
 const stepASection = document.getElementById('games-stepA');
 const stepBSection = document.getElementById('games-stepB');
+const playerNameLocation = document.getElementById('playerName');
+const playerScoreLocation = document.getElementById('playerScore');
 const pathName = window.location.pathname;
 const gameId = pathName.split('/').at(-1);
+let gameCells;
 let charList;
+let usersList;
 let playersList = [];
 let finalPlayersList = [];
 let gameLevel = 0;
+let firstCardSelected = null;
+let playerScores = [];
+let playerTurn = 0;
 
 usersFields.forEach((userField) => {
     userField.addEventListener('click', () => {
         // If the user is checked, add it to the players list
         if(userField.checked){
+            userField.nextElementSibling.classList.add('isActive');
             playersList.push(userField.value);
         }else{// else, remove it from the players list
             let playerIndex = playersList.indexOf(userField.value);
             if (playerIndex > -1) {
+                userField.nextElementSibling.classList.remove('isActive');
                 playersList.splice(playerIndex, 1);
             }
         }
@@ -52,20 +61,60 @@ startBtn.addEventListener('click', (event) => {
         }
     });
 
+    playersList = finalPlayersList;
+
     initGame();
-    /*console.log(finalPlayersList);
-    console.log(gameLevel);*/
 });
 
 function initGame(){
+    for(let player of playersList){
+        playerScores.push(0);
+    }
+
     if(gameId == 2){
-        // this value is used to always have an even number of minimum character's number
-        let addValue = parseInt(gameLevel)-1;
-        let colsNumber = parseInt(gameLevel)+addValue+3;
+        playerNameLocation.innerHTML = usersList[playersList[playerTurn]]['name'];
+        if(!playerNameLocation.classList.contains('color-'+usersList[playersList[playerTurn]]['color'])){
+            playerNameLocation.classList.add('color-'+usersList[playersList[playerTurn]]['color']);
+        }
+
+        let colsNumber;
+        let rowsNumber;
+        let cellWidth;
+        switch(gameLevel){
+            case '1':
+                colsNumber = 4;
+                rowsNumber = 4;
+                cellWidth = 'A';
+                break;
+            case '2':
+                colsNumber = 8;
+                rowsNumber = 4;
+                cellWidth = 'B';
+                break;
+            case '3':
+                colsNumber = 8;
+                rowsNumber = 8;
+                cellWidth = 'C';
+                break;
+            case '4':
+                colsNumber = 10;
+                rowsNumber = 10;
+                cellWidth = 'D';
+                break;
+            case '5':
+                colsNumber = 16;
+                rowsNumber = 9;
+                cellWidth = 'D';
+                break;
+            default:
+                colsNumber = 8;
+                rowsNumber = 4;
+                cellWidth = 'A';
+        }
 
         // count the minimum number of characters needed to make the game work
-        let minCharNeeded = Math.ceil((colsNumber * colsNumber)/2);
-        console.log(minCharNeeded);
+        let minCharNeeded = Math.ceil((colsNumber * rowsNumber)/2);
+
         if(charList.length < minCharNeeded){
             alert('Erreur : Pas assez de personnages enregistrés pour pouvoir jouer à ce jeu! Il faut un minimum de '+minCharNeeded+' personnages pour que ce jeu puisse fonctionner correctement. Veuillez donc ajouter de nouveaux personnages puis réessayer.')
         }else{
@@ -75,31 +124,131 @@ function initGame(){
             if(stepBSection.classList.contains('hidden')){
                 stepBSection.classList.remove('hidden');
             }
-        }
 
-        let randomCharList = shuffleArray(charList);
-        let slicedArray = randomCharList.slice(0, minCharNeeded);
-        let gameCards = slicedArray.concat(slicedArray);
-        gameCards = shuffleArray(gameCards);
-        console.log(gameCards);
+            let randomCharList = shuffleArray(charList);
+            let slicedArray = randomCharList.slice(0, minCharNeeded);
+            let gameCards = slicedArray.concat(slicedArray);
+            gameCards = shuffleArray(gameCards);
 
-        let gameContainer = document.getElementById('memory-stepB-container');
-        let gameArea = document.createElement('table');
-        let tableContent = '';
-        let charCounter = 0;
-        
-        for(let i=0; i<colsNumber; i++){
-            tableContent += '<tr>';
-            for(let y=0; y<colsNumber; y++){
-                tableContent += '<td><img id="character_'+charCounter+'" src="/img/characters/'+gameCards[charCounter]['img']+'" alt="Image not found"></td>';
-                charCounter++;
+            let gameContainer = document.getElementById('memory-stepB-container');
+            let gameDisabler = document.createElement('div');
+            gameDisabler.classList.add('memory-disabler');
+            let gameArea = document.createElement('table');
+            gameArea.classList.add('memory-table');
+            let tableContent = '';
+            let charCounter = 0;
+            
+            for(let i=0; i<rowsNumber; i++){
+                tableContent += '<tr>';
+                for(let y=0; y<colsNumber; y++){
+                    tableContent += '<td id="card_'+charCounter+'" class="memory-cell level'+cellWidth+'"><img class="memory-backCard" src="/img/memory-backCard.jpg"><img id="character_'+charCounter+'" class="memory-card" src="/img/characters/'+gameCards[charCounter]['img']+'" alt="Image not found"></td>';
+                    charCounter++;
+                }
+                tableContent += '</tr>';
             }
-            tableContent += '</tr>';
+
+            gameDisabler.innerHTML = '.';
+            gameArea.innerHTML = tableContent;
+            
+            gameContainer.appendChild(gameDisabler);
+            gameContainer.appendChild(gameArea);
+
+            gameCells = document.getElementsByClassName('memory-backCard');
+            for(let cell of gameCells){
+                cell.addEventListener('click', (e) => {
+                    let cardId = e.path[1].id.split('_')[1];
+                    //let test = document.querySelector('#card_'+cardId)
+                    e.target.style.display = 'none';
+                    // check if the first card already has been selected or not
+                    if(!firstCardSelected){
+                        firstCardSelected = cardId;
+                    }else{
+                        // check if the second card is not the same as the first card
+                        if(firstCardSelected != cardId){
+                            // if the characters are the same on both cards, the player obtain 1 point
+                            if(gameCards[firstCardSelected]['id'] == gameCards[cardId]['id']){
+                                playerScores[playerTurn]++;
+                                // if it's not the last player to play, it's the next player's turn
+                                /*
+                                if(playerNameLocation.classList.contains('color-'+usersList[playersList[playerTurn]]['color'])){
+                                    playerNameLocation.classList.remove('color-'+usersList[playersList[playerTurn]]['color']);
+                                }
+                                if(playerTurn < playersList.length-1){
+                                    playerTurn++;
+                                }else{// else it's the first player to play
+                                    playerTurn = 0;
+                                }
+                                playerNameLocation.innerHTML = usersList[playersList[playerTurn]]['name'];
+                                if(!playerNameLocation.classList.contains('color-'+usersList[playersList[playerTurn]]['color'])){
+                                    playerNameLocation.classList.add('color-'+usersList[playersList[playerTurn]]['color']);
+                                }*/
+
+                                let remainingCards = 0;
+                                for(let card of gameCells){
+                                    if(card.style.display != 'none'){
+                                        remainingCards++;
+                                    }
+                                }
+
+                                if(remainingCards < 1){
+                                    let endMessage = document.createElement('div');
+                                    endMessage.classList.add('gameResults');
+                                    let finalResults = [];
+                                    for(let score of playerScores){
+                                        finalResults.push([playersList[playerScores.indexOf(score)],score]);
+                                    }
+                                    finalResults.sort((a, b) => {
+                                        return a[1] - b[1];
+                                    });
+                                    finalResults = finalResults.reverse();
+                                    
+                                    let endMsgContent = '<p>GAGANT : <span class="player-info color-'+usersList[finalResults[0][0]]['color']+'">'+usersList[finalResults[0][0]]['name']+'</span> avec <span class="player-info">'+finalResults[0][1]+'</span> points, Félicitations!</p>';
+                                    if(playersList.length > 1){
+                                        endMsgContent += '<p>Scores des autres joueurs :</p>';
+                                        endMsgContent += '<ul class="gameResults-list">';
+                                        for(let i=1;i<finalResults.length;i++){
+                                            endMsgContent += '<li>'+usersList[finalResults[i][0]]['name']+' => '+finalResults[i][1]+'</li>'
+                                        }
+                                        endMsgContent += '</ul>';
+                                    }
+                                    endMsgContent += '<a href="/games/2" class="kidsMoviesBtn greenBG">Rejouer</a>'
+
+                                    endMessage.innerHTML = endMsgContent;
+                                    stepBSection.appendChild(endMessage);
+                                }else{
+                                    playerScoreLocation.innerHTML = playerScores[playerTurn];
+                                    firstCardSelected = null;
+                                }
+                            }else{
+                                let disabler = document.getElementsByClassName('memory-disabler')[0];
+                                disabler.style.display = 'block';
+                                // Wait 2 seconds then hide the cards again
+                                setTimeout(() => {
+                                    document.getElementById('card_'+firstCardSelected).firstChild.style.display = 'block';
+                                    e.target.style.display = 'block';
+                                    disabler.style.display = 'none';
+                                    if(playerNameLocation.classList.contains('color-'+usersList[playersList[playerTurn]]['color'])){
+                                        playerNameLocation.classList.remove('color-'+usersList[playersList[playerTurn]]['color']);
+                                    }
+                                    // if it's not the last player to play, it's the next player turn
+                                    if(playerTurn < playersList.length-1){
+                                        playerTurn++;
+                                    }else{// else it's the first player to play
+                                        playerTurn = 0;
+                                    }
+                                    playerNameLocation.innerHTML = usersList[playersList[playerTurn]]['name'];
+                                    if(!playerNameLocation.classList.contains('color-'+usersList[playersList[playerTurn]]['color'])){
+                                        playerNameLocation.classList.add('color-'+usersList[playersList[playerTurn]]['color']);
+                                    }
+                                    playerScoreLocation.innerHTML = playerScores[playerTurn];
+                                    firstCardSelected = null;
+                                }, 2000)
+                            }
+                        }
+                    }
+                });
+            }
         }
-
-        gameArea.innerHTML = tableContent;
-
-        gameContainer.appendChild(gameArea);
     }
 }
 
@@ -112,20 +261,29 @@ function initGame(){
     charList = charactersList;
 }
 
+/**
+ * Store the current users list
+ * 
+ * @param {[object]} usersList 
+ */
+ export function setUsersList(users){
+    usersList = users;
+}
+
 function shuffleArray(array) {
     let currentIndex = array.length,  randomIndex;
-  
+
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
-  
+
     return array;
-  }
+}
--- views/content/game.php
+++ views/content/game.php
@@ -3,11 +3,13 @@
     $users = $params['users'];
     $characters = $params['characters'];
     $jsonCharactersList = json_encode($characters);
+    $jsonUsersList = json_encode($users);
 ?>
 <script type="module">
-    import {setCharactersList} from '../../public/scripts/games.js';
+    import {setCharactersList, setUsersList} from '../../public/scripts/games.js';
     // In order to make the character's sheet work, we need to pass the characters data to JS
     setCharactersList(<?= $jsonCharactersList ?>);
+    setUsersList(<?= $jsonUsersList ?>);
 </script>
 
 <section class="section whiteBG">
@@ -23,29 +25,33 @@
                 <div>
                     <h4>Qui va jouer ?</h4>
                     <?php foreach($users as $key => $user): ?>
-                        <input type="checkbox" id="players-<?= $key ?>" value="<?= $key ?>" name="playersList"><label for="players-<?= $key ?>"><?= substr(strtoupper($user['name']), 0, 1) ?></label>
+                        <input type="checkbox" id="players-<?= $key ?>" value="<?= $key ?>" name="playersList" class="player-field"><label class="player-selection userColor_<?= $user['color'] ?>" for="players-<?= $key ?>"><?= substr(strtoupper($user['name']), 0, 1) ?></label>
                     <?php endforeach; ?>
                 </div>
                 <div>
                     <h4>Niveau de difficulté :</h4>
-                    <input type="radio" id="level-veryEasy" value="1" name="level"><label for="level-veryEasy">Très facile</label>
-                    <input type="radio" id="level-easy" value="2" name="level" checked="checked"><label for="level-easy">Facile</label>
-                    <input type="radio" id="level-medium" value="3" name="level"><label for="level-medium">Normal</label>
-                    <input type="radio" id="level-hard" value="4" name="level"><label for="level-hard">Difficile</label>
-                    <input type="radio" id="level-extreme" value="5" name="level"><label for="level-extreme">Extrême</label>
+                    <input type="radio" id="level-veryEasy" value="1" name="level" class="gameLevels"><label for="level-veryEasy">Très facile</label>
+                    <input type="radio" id="level-easy" value="2" name="level" checked="checked" class="gameLevels"><label for="level-easy">Facile</label>
+                    <input type="radio" id="level-medium" value="3" name="level" class="gameLevels"><label for="level-medium">Normal</label>
+                    <input type="radio" id="level-hard" value="4" name="level" class="gameLevels"><label for="level-hard">Difficile</label>
+                    <input type="radio" id="level-extreme" value="5" name="level" class="gameLevels"><label for="level-extreme">Extrême</label>
                 </div>
                 <button class="kidsMoviesBtn blueBG" id="startGame" disabled>Commencer</button>
             </form>
         </div>
         <?php if($game->getTitle() == 'Blind test'): ?>
             <div id="games-stepB" class="blindTest-stepB hidden">
+                <a href="/games/<?= $game->getId() ?>">Retour</a>
                 <div id="blindTest-stepB-container">
                     <div id="blindTest-stepB-videoPlayer">Vidéo</div>
                 </div>
             </div>
         <?php elseif($game->getTitle() == 'Memory'): ?>
             <div id="games-stepB" class="memory-stepB hidden">
-                <div id="memory-stepB-container"></div>
+                <a href="/games/<?= $game->getId() ?>" class="backLink">Retour</a>
+                <p>Au tour de <span id="playerName" class="player-info"></span></p>
+                <p>Score actuel : <span id="playerScore" class="player-info">0</span> point(s)</p>
+                <div id="memory-stepB-container" class="memory-stepB-container"></div>
             </div>
         <?php endif; ?>
     </div>
